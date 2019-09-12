e5d81769247349952bb0a3f0eb54eb399bf8f462
Ivy Leano <IvyLeano@outlook.com>
2019-09-12 11:52:32 -0400

    Update App.js

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,10 +1,6 @@
 /*********************************************************************************
 * WEB422 – Assignment 04
-* I declare that this assignment is my own work in accordance with Seneca Academic Policy. No part of this
-* assignment has been copied manually or electronically from any other source (including web sites) or
-* distributed to other students.
-*
-* Name: __Ivy leano-hill_____ Student ID: __120331186____________ Date: __2019-07-14______________
+* Date: __2019-07-14______________
 *
 ********************************************************************************/
 import React, { Component } from 'react';
